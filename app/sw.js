c79c019a87700abf075ae856a707705f8820a8e4
Vincent <vkammerer@gmail.com>
2015-05-08 17:13:57 -0400

    Refactors and adds cache

---
+++ app/sw.js
@@ -0,0 +1,48 @@
+importScripts('../bower_components/cache-polyfill/index.js');
+
+var initCache = function initCache () {
+	caches.open('v1').then(function(cache) {
+		return cache.addAll([
+			'/app/',
+			'/app/index.html',
+			'/app/style.css',
+			'/app/script.js'
+		]);
+	})
+}
+
+self.addEventListener('install', function(event) {
+	console.log('install', event);
+	event.waitUntil(
+		initCache()
+	);
+});
+
+self.addEventListener('activate', function(event) {
+	console.log('activate', event);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+})
+
+/*
+	Custom response example
+*/
+
+/*
+self.addEventListener('fetch', function(event) {
+  console.log('fetch', event);
+	if (/\.css$/.test(event.request.url)) {
+		event.respondWith(
+			new Response('body { color: red; }', {
+			  headers: { 'Content-Type': 'text/css' }
+			})
+		);
+	}
+});
+*/
